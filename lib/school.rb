056e8b89fc5c83a777cd7eb9fd3e7d923f637c9b
metrio <dlima.nyit@gmail.com>
2020-10-11 15:36:19 -0400

    Message

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name 
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        roster[grade] ||= []
+        roster[grade] << student_name
+    end
+
+    def grade(year)
+        return roster[year]
+    end
+
+    def sort
+    sorted = {}
+        roster.map do |grade, students|
+            sorted[grade] = students.sort
+        end
+    return sorted
+    end
+
+end
+
